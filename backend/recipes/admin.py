2cf8233f926cd2755f1c8486f3ed13e8ce2e49ac
VeronikaLapteva <v.a.strelnikova@mail.ru>
2023-10-04 16:01:56 +0300

    admin recipes

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.core.exceptions import ValidationError
 
 from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                     ShoppingList, Tag)
+                     RecipeTag, ShoppingList, Tag)
 
 admin.site.register(Tag)
 admin.site.register(Ingredient)
@@ -13,21 +12,15 @@ admin.site.register(ShoppingList)
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
     extra = 1
+    min_num = 1
+
+
+class RecipeTagtInline(admin.TabularInline):
+    model = RecipeTag
+    extra = 0
+    min_num = 1
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = (RecipeIngredientInline, )
-
-    def validate(self, data):
-        if not data.get('recipe_ingredients'):
-            raise ValidationError(
-                'Нужно добавить в рецепт хотя бы один ингредиент!')
-        for ingredient in data.get('recipe_ingredients'):
-            if ingredient.get('amount') < 0:
-                raise ValidationError(
-                    'Количество ингредиентов должно быть не меньше одного!')
-        if not data.get('tags'):
-            raise ValidationError(
-                'Нужно выбрать хотя бы один тег!')
-        return data
+    inlines = (RecipeIngredientInline, RecipeTagtInline,)
