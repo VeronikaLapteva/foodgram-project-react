ec145d5df661fe585a3273fef94ec5df5ead1ccd
VeronikaLapteva <v.a.strelnikova@mail.ru>
2023-10-04 23:01:57 +0300

    updated views

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,14 +4,13 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
+from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                            ShoppingList, Tag)
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-
-from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                            ShoppingList, Tag)
 from users.models import Subscription
 
 from .filters import RecipeFilter
